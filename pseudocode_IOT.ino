1e27085d931fa42b20c5e0ec969e0366d2765b74
nuriaulfah <114793248+nuriaulfah@users.noreply.github.com>
2024-01-15 09:54:19 +0700

    Add files via upload

---
+++ pseudocode_IOT.ino
@@ -0,0 +1,32 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+if(!Serial) Serial.println("serial is not ok");
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(3, HIGH);
+Serial.println("red is on");
+delay(600);
+digitalWrite(3, LOW);
+Serial.println("red is off");
+digitalWrite(4, HIGH);
+Serial.println("yellow is on");
+delay(600);
+digitalWrite(4, LOW);
+Serial.println("yellow is off");
+digitalWrite(5, HIGH);
+Serial.println("green is on");
+delay(5000);
+digitalWrite(4, HIGH);
+Serial.println("yellow is on");
+delay(500);
+digitalWrite(3, LOW);
+Serial.println("red is off");
+digitalWrite(4, LOW);
+Serial.println("yellow is off");
+}
